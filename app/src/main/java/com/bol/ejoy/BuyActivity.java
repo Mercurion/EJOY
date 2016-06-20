18033daea953f304070dab1c24f549461b60782c
Mercurion <jackbianchini@gmail.com>
2016-06-20 10:53:26 +0200

    commit

--- app/src/main/java/com/bol/ejoy/BuyActivity.java
+++ app/src/main/java/com/bol/ejoy/BuyActivity.java
@@ -1,19 +1,83 @@
 package com.bol.ejoy;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by jackb on 05/06/2016.
  */
 public class BuyActivity extends Activity {
 
+    //the images to display
+    Integer[] imageIDs = {
+            R.drawable.ejoy_classic1,
+            R.drawable.ejoy_classic2,
+            R.drawable.ejoy_classic3,
+            R.drawable.ejoy_green_gold_luxury402,
+            R.drawable.ejoy_green_gold_luxury408,
+            R.drawable.ejoy_green_gold_luxury410,
+            R.drawable.ejoy_green_gold_luxury425
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buy);
+        setContentView(R.layout.buy_activity);
+
+        // Note that Gallery view is deprecated in Android 4.1---
+        Gallery gallery = (Gallery) findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                Toast.makeText(getBaseContext(),"pic" + (position + 1) + " selected",
+                        Toast.LENGTH_SHORT).show();
+                // display the images selected
+                ImageView imageView = (ImageView) findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
     }
 
-
-
+    public class ImageAdapter extends BaseAdapter {
+        private Context context;
+        private int itemBackground;
+        public ImageAdapter(Context c)
+        {
+            context = c;
+            // sets a grey background; wraps around the images
+            TypedArray a =obtainStyledAttributes(R.styleable.MyGallery);
+            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+        // returns the number of images
+        public int getCount() {
+            return imageIDs.length;
+        }
+        // returns the ID of an item
+        public Object getItem(int position) {
+            return position;
+        }
+        // returns the ID of an item
+        public long getItemId(int position) {
+            return position;
+        }
+        // returns an ImageView view
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+    }
 }
+
